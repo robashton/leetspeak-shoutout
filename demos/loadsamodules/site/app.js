2eb6c05d89c85f69ec39a4451a63d55c31055032
Rob Ashton <robashton@codeofrob.com>
2012-10-19 20:18:29 +0200

    simple code innnit

--- demos/loadsamodules/site/app.js
+++ demos/loadsamodules/site/app.js
@@ -1,17 +1,13 @@
 $(function() {
   var $name = $("input[name='name']") 
   var $shoutout = $("input[name='shoutout']")
+
   $("#submit").click(function() {
       var name = $name.val()
       var shoutout = $shoutout.val()
 
-      $.ajax({
-        url: '/',
-        type: 'POST',
-        data: { name: name, shoutout: shoutout },
-        success: function(html) {
-          $('#shoutouts').html(html)
-        }
+      $('#shoutouts').load('/', {
+         name: name, shoutout: shoutout 
       })
       $shoutout.val('')
   })
